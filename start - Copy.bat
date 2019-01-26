2ef09bc5ce28c61c0b82fb4d9ea05d03f8c1faf9
Mia5aLifa123xNxx <45575591+Mia5aLifa123xNxx@users.noreply.github.com>
2019-01-26 01:08:37 +0100

    Add files via upload

---
+++ start - Copy.bat
@@ -0,0 +1,10 @@
+@echo off 
+color e
+@echo off
+echo Hello , Welcome Sir %username%
+echo This Code By Kahrbaa , [overHype].
+@echo off
+start /max https://discord.gg/eYBrqDJ"
+cd bot
+node bot.js
+pause
